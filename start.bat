61125d7388b2941887d2de7062f71e60ef039eeb
Anthony Marinov <anthonymmarinov@gmail.com>
2025-03-31 17:22:08 -0700

    start batch file updated

--- start.bat
+++ start.bat
@@ -1,6 +1,8 @@
 @echo off
 cd /d D:\my-files\coding-projects\vanilla-todo-app\backend\src
-start http://localhost:8080 &
-start /b java -cp ".;../libs/*" Main &
+start /b java -cp ".;../libs/*" Main
 start /b python -m http.server 8080 --directory "D:\my-files\coding-projects\vanilla-todo-app\frontend"
-pause
+start "Todo App" "http://localhost:8080"
+
+echo Todo app started.
+pause >nul
