dfd693b5e7544c139684c3f31ddf7c54a047f8de
Anthony Marinov <anthonymmarinov@gmail.com>
2025-03-31 18:37:31 -0700

    feature: start.bat batch script updated

--- start.bat
+++ start.bat
@@ -1,7 +1,15 @@
 @echo off
-cd /d D:\my-files\coding-projects\vanilla-todo-app\src\backend
+
+REM Edit this path to the location of vanilla-todo-app on your system
+set basePath=D:\my-files\coding-projects\vanilla-todo-app
+
+set backendPath=\src\backend
+set frontendPath=\src\frontend
+set fullBackendPath=%basePath%%backendPath%
+set fullFrontendPath=%basePath%%frontendPath%
+cd /d %fullBackendPath%
 start /b java -cp ".;libs/*" Main
-start /b python -m http.server 8080 --directory "D:\my-files\coding-projects\vanilla-todo-app\src\frontend"
+start /b python -m http.server 8080 --directory %fullFrontendPath%
 start "Todo App" "http://localhost:8080"
 
 echo Todo app started.
